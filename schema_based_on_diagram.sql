43a22a59f01176e2e18543c71b95c8febaf23520
29td <nonhlanhlamndebele9@gmail.com>
2022-09-27 18:11:04 +0200

    create join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,10 @@ CREATE TABLE invoice_items (
 	invoice_id INTEGER REFERENCES invoices(id),
 	treatment_id INTEGER REFERENCES treatments(id)
 );
+
+-- join table
+
+CREATE TABLE medial_treatments (
+	medical_history_id INTEGER REFERENCES medical_histories(id),
+	treatment_id INTEGER REFERENCES treatments(id)
+);
